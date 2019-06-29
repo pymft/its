86638d98e8a5c0fe66786e3439e399097f9ff1fb
iamvee <naeini.v@gmail.com>
2019-06-30 02:14:35 +0430

    polynomial part 2 (repr)  added

--- oop/polynomial/src/part_2.py
+++ oop/polynomial/src/part_2.py
@@ -1,27 +1,17 @@
-# Polynomial
-
-## Part 1
-
-### main frame
-
-```python
->>> # create a callable object for f(x) := x^2 + 2x + 3
->>> eq = Polynomial([1, 2, 3])
->>> # check degree
->>> eq.degree
-2
->>> # check its coefficients
->>> eq.coefficients
-(1, 2, 3)
->>> # and its value for x = 2 will be
->>> eq(2)   # 1 * 2 ** 2 + 2 * 2 ** 1 + 3 * 2 ** 
-11
-```
-
-### solution 
-
-```python 
 class Polynomial:
+    """ creates polynomial objects 
+    
+    >>> eq = Polynomial([1, 2, 3])
+    >>> eq.degree
+    2
+    >>> eq.coefficients
+    (1, 2, 3)
+    >>> eq(2)
+    11
+    >>> Polynomial([8, 300, -2, 2, -3])
+       4      3    2    1    0
+    +8x  +300x  -2x  +2x  -3x
+    """
     def __init__(self, coefficients):
         self.__coefficients = tuple(coefficients)
         self.__degree = len(coefficients) - 1 
@@ -39,26 +29,7 @@ class Polynomial:
         for k, c in zip(range(self.degree, -1, -1), self.coefficients):
             value += c * x ** k
         return value
-```
 
-## Part2
-
-### represent it
-```python
->>> eq = Polynomial([8, 300, -2, 2, -3])
->>> eq
-   4      3    2    1    0
-+8x  +300x  -2x  +2x  -3x
-```
-
-### solution 
-
-add `__repr__` method
-```python
-class Polynomial:
-    # ...
-    # Part 1
-    # ...
     def __repr__(self):
         white_area = 2
         white_str = " " * white_area
@@ -66,5 +37,8 @@ class Polynomial:
         coef_whitespace = list(map(lambda c: " "*(len(c)-2), coef))
         pows = list(map(lambda i: f"{coef_whitespace[i]}{self.degree - i:2}", range(self.degree+1)))
         return " " + white_str.join(pows) + "\n" + white_str.join(coef)
-```
 
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
