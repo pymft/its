22b97dbae7ae737fa3a265ebc65e7051de1578f7
iamvee <naeini.v@gmail.com>
2019-06-30 06:00:02 +0430

    compute values

--- flask_apps/bmi/app.py
+++ flask_apps/bmi/app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        weight = float(request.form.get('weight'))
+        height = float(request.form.get('height'))
+        name = request.form.get('name')
+        bmi = weight / height ** 2
         result = 25   # temporary value to check if it's working just OK 
-        return render_template('index.html', result=result)
+        return render_template('index.html', result=bmi, name=name)
     return render_template('index.html')
 
     
